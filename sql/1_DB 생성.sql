a6750ed6998163d32bc25a7a1bd07a741015c245
kbw <rhtmaehcl878@naver.com>
2026-05-09 11:40:02 +0900

    프로젝트 초기 설정 및 DB 생성 스크립트 추가

---
+++ sql/1_DB 생성.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE market_board
+DEFAULT CHARACTER SET utf8mb4 -- MySQL에서 한글, 이모지, 다양한 유니코드 문자를 안정적으로 저장할 수 있는 문자셋
+COLLATE utf8mb4_unicode_ci; -- 문자 비교/정렬 기준
---
+++ src/main/java/com/buildenough/marketboard/MarketBoardApplication.java
@@ -0,0 +1,13 @@
+package com.buildenough.marketboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBoardApplication.class, args);
+	}
+
+}
